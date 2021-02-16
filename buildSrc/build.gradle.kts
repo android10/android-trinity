431690d055c75bb31da76ef59deb10c7573bfd16
Fernando Cejas <fcejas@gmail.com>
2021-02-16 21:30:10 +0100

    Bump detekt dependency.

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,7 +1,7 @@
 object Dependencies {
     const val AndroidBuildTools = "com.android.tools.build:gradle:4.1.0"
     const val kotlinGradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.10"
-    const val detektGradlePlugin = "io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.14.1"
+    const val detektGradlePlugin = "io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.15.0"
 }
 
 plugins {
