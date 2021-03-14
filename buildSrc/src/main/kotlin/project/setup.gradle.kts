8da3b8ae4846d81e369089b51e2bdb3ff3ba01d0
Fernando Cejas <fcejas@gmail.com>
2021-03-14 23:15:47 +0100

    Add logic to validate package name and list files with default package name content.

--- buildSrc/src/main/kotlin/project/setup.gradle.kts
+++ buildSrc/src/main/kotlin/project/setup.gradle.kts
@@ -15,9 +15,62 @@
  */
 package project
 
-import org.gradle.kotlin.dsl.register
+import java.io.File
+import java.io.File.separator as SEPARATOR
+
+val defaultPackageName = "com.fernandocejas.sample"
+val projectPath: String = rootProject.rootDir.absolutePath
+val directoryList = listOf(
+    File(projectPath.plus(SEPARATOR).plus("app")),
+    File(projectPath.plus(SEPARATOR).plus("buildSrc").plus(SEPARATOR).plus("src")))
 
 tasks.register("setupProject") {
     description = "Sets up the project. A valid package name should be passed as argument"
-    println("Setup Project...")
+
+    // Check valid package name
+    if (validatePackageName("com.java")) {
+        directoryList.map { directory ->
+            // Change package name in source code
+            directory
+                .walk()
+                .filter { filterFile(it) }
+                .filter { filterContent(it) }
+                .forEach { println(it) }
+                //.forEach { it.readText().replace(defaultPackageName, "newPackageName".toLowerCase()) }
+
+            // Rename package directories
+
+
+            // Clean up code by removing leftover from this script
+
+        }
+    } else {
+        println("Wrong package name")
+    }
+}
+
+fun filterFile(file: File): Boolean {
+    if (file.name.startsWith("setup.gradle")) return false
+
+    if (file.name.startsWith("AndroidManifest")) return true
+    if (file.extension == "kts" || file.extension == "kt") return true
+
+    return false
+}
+
+fun filterContent(file: File): Boolean {
+    if (file.readText().contains(defaultPackageName)) return true
+    return false
+}
+
+fun validatePackageName(packageName: String): Boolean {
+    // This regex ensures that the package name:
+    // - can only be formed by letters, digits, dots and underscores.
+    // - must always start with a word (or with an underscore), but never with a digit or a dot.
+    // - can optionally contain a sequence of a pattern made by: a dot, followed by a word that
+    //   can start with letters and underscore but not with digits.
+    // - must always end with a non-dot character.
+    // - must start with a lowercase letter by convention.
+    val regex = "(^(?:[a-z_]+(?:\\d*[a-zA-Z_]*)*)(?:\\.[a-z_]+(?:\\d*[a-zA-Z_]*)*)*\$)".toRegex()
+    return regex.matches(packageName)
 }
